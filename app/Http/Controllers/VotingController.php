f68053de87ba7253f0ad38d5ef2276f85ed33485
Yaron Tal <yaron.tal@oneshoe.nl>
2016-11-24 11:52:30 +0100

    Created simple index listing all songs with a user's votes if logged in.

---
+++ app/Http/Controllers/VotingController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Song;
+use Illuminate\Support\Facades\Auth;
+
+class VotingController extends Controller
+{
+    public function index() {
+        if (Auth::check()) {
+            $songs = Song::with([
+                'vote' => function ($query) {
+                    $query->where('user_id', Auth::user()->id);
+                }
+            ])->get();
+        } else {
+            $songs = Song::all();
+        }
+
+        return view('index')->with([
+            'songs' => $songs,
+        ]);
+    }
+}
