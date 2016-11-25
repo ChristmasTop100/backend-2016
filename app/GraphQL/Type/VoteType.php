690c360cd66c8e56d30b41dd7921ced8d318df09
Yaron Tal <yaron.tal@oneshoe.nl>
2016-11-25 19:03:35 +0100

    Fixed link from votes back to users and songs

--- app/GraphQL/Type/VoteType.php
+++ app/GraphQL/Type/VoteType.php
@@ -4,27 +4,36 @@ namespace App\GraphQL\Type;
 
 use GraphQL\Type\Definition\Type;
 use Folklore\GraphQL\Support\Type as GraphQLType;
+use Folklore\GraphQL\Support\Facades\GraphQL;
 
 class VoteType extends GraphQLType {
   protected $attributes = [
-    'name' => 'Vote',
-    'description' => 'A vote by a user'
+      'name' => 'Vote',
+      'description' => 'A vote by a user'
   ];
 
   public function fields()
   {
     return [
       'song_id' => [
-        'type' => Type::nonNull(Type::int()),
-        'description' => 'The id of the song'
+          'type' => Type::nonNull(Type::int()),
+          'description' => 'The id of the song'
       ],
       'user_id' => [
-        'type' => Type::nonNull(Type::int()),
-        'description' => 'The id of the user'
+          'type' => Type::nonNull(Type::int()),
+          'description' => 'The id of the user'
       ],
       'score' => [
-        'type' => Type::int(),
-        'description' => 'The score given by the user'
+          'type' => Type::int(),
+          'description' => 'The score given by the user'
+      ],
+      'users' => [
+          'type' => GraphQL::type('User'),
+          'description' => 'The user that voted'
+      ],
+      'songs' => [
+          'type' => GraphQL::type('Song'),
+          'description' => 'The song that was voted on'
       ]
     ];
   }
--- app/Vote.php
+++ app/Vote.php
@@ -14,4 +14,12 @@ class Vote extends Model
     protected $fillable = [
         'user_id', 'song_id', 'score',
     ];
+
+  public function users() {
+    return $this->hasOne('App\User', 'id', 'user_id');
+  }
+
+  public function songs() {
+    return $this->hasOne('App\Song', 'id', 'song_id');
+  }
 }
