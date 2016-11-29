dfae7e97dc602cc82b3c79a86811b76e63390e10
Robin Drost <robindrost@MacBook-Pro-van-Robin.local>
2016-11-29 21:24:32 +0100

    Added job schedule for the spotify import.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('spotify:import')->daily();
     }
 }
