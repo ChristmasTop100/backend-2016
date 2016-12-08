1444c3a608aaf6c9b9fa916658e520e8927b9f80
Yaron Tal <yaron.tal@oneshoe.nl>
2016-12-08 18:13:59 +0100

    Extended UpdateUserMutation to also allow users to set a new password with an email address and login token.

--- app/GraphQL/Query/UpdateUserMutation.php
+++ app/GraphQL/Query/UpdateUserMutation.php
@@ -7,34 +7,71 @@ use Folklore\GraphQL\Support\Mutation;
 use GraphQL\Type\Definition\Type;
 use Illuminate\Support\Facades\Auth;
 use JWTAuth;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class UpdateUserMutation extends Mutation{
-  protected $attributes = [
-    'name' => 'UpdateUser'
-  ];
-
-  public function type()
-  {
-    return GraphQL::type('User');
-  }
-
-  public function args()
-  {
-    return [
-      'password' => ['name' => 'password', 'type' => Type::nonNull(Type::string())]
+    protected $attributes = [
+        'name' => 'UpdateUser'
     ];
-  }
 
-  public function resolve($root, $args)
-  {
-    if (! Auth::check()) {
-      Throw new GraphQL\Error('You need to be logged in to change a password.');
+    public function type()
+    {
+        return GraphQL::type('User');
     }
 
-    $user = Auth::user();
-    $user->password = Hash::make($args['password']);
-    $user->save();
-    return $user;
-  }
+    public function args()
+    {
+        return [
+            'password' => ['name' => 'password', 'type' => Type::string()],
+            'email' => ['name' => 'email', 'type' => Type::string()],
+            'token' => ['name' => 'token', 'type' => Type::string()]
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        if (! Auth::check() && ! isset($args['token'])) {
+            Throw new GraphQL\Error('You need to be logged in to change a password.');
+        }
+
+        if (isset($args['token'])) {
+            if ( ! isset($args['email']) || ! isset($args['password'])) {
+                Throw new GraphQL\Error('New password and current mail needed to use login token.');
+            }
+            $credentials = $args + ['password_confirmation' => $args['password']];
+            $response = Password::broker()->reset($credentials, function ($user, $password) {
+                $this->resetPassword($user, $password);
+
+            });
+
+            if ($response != Password::PASSWORD_RESET) {
+                Throw new GraphQL\Error('Invalid token');
+            }
+            $user = Auth::user();
+        }
+        else {
+            $user = Auth::user();
+            $this->resetPassword($user, $args['password']);
+        }
+
+        return $user;
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $user->forceFill([
+          'password' => bcrypt($password),
+          'remember_token' => Str::random(60),
+        ])->save();
+
+        Auth::guard()->login($user);
+    }
 }
