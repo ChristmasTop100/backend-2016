8d092695a9b1ae84a0f9d67fad9869e9aa33a3e3
Yaron Tal <yaron.tal@oneshoe.nl>
2016-12-11 21:38:37 +0100

    Typo fix in updatevote mutation

--- app/GraphQL/Query/UpdateVoteMutation.php
+++ app/GraphQL/Query/UpdateVoteMutation.php
@@ -46,7 +46,7 @@ class UpdateVoteMutation extends Mutation{
     if (($scores->sum() + $args['score']) > 100) {
       Throw new GraphQL\Error('The sum of all your votes may not exceed 100.');
     }
-    
-    return Vote::updateOrCreate(compact(['song_id', 'user_id']), [['score'] => $args['score']]);
+
+    return Vote::updateOrCreate(compact(['song_id', 'user_id']), ['score' => $args['score']]);
   }
 }
