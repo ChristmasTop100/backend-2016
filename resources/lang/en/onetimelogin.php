8de5202dfa5c2d4f8c18810c2783fa7262ca3a29
robindrost <robindrost@gmail.com>
2016-12-12 11:42:13 +0100

    Update onetimelogin.php

--- resources/lang/en/onetimelogin.php
+++ resources/lang/en/onetimelogin.php
@@ -7,6 +7,6 @@ return [
 	'token_text' => 'From now on you have an account on christmastop100.nl. We created a one time login link for you: :otl_link.',
 	'desc_text' => 'On the website you\'ll see a long list of christmas songs. You\'ll get 100 points, which you can distribute between your favorite songs! At the end of the week we\'ll make a top 100 based on all scores and have a whole day filled with the best christmas songs! Is that the recipe for a jolly day or what?',
 	'closing' => 'Greetings,',
-  'sender_name' => 'The christmas team (Robin and Yaron).',
+  'sender_name' => 'The christmas team (Robin, Dylan, David, and Yaron).',
 	'ps' => 'PS. If you see any bugs or have ideas for improvements, report them or create a pull request on https://github.com/ChristmasTop100',
 ];
